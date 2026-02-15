5bc5a8dafc7956acc0f355392b8fa19048b87110
unbornchicken-voices <95977017+unbornchicken-voices@users.noreply.github.com>
2026-02-15 14:31:13 +0100

    site undo

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['Literata', 'serif'],
+        // Këtu regjistrojmë fontin tënd të ri
+        news: ['"Archivo"', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 }
