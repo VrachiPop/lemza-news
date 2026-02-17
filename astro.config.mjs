eecea19e02b06f1c45fa6a7b27b01fd73b22921f
unbornchicken-voices <95977017+unbornchicken-voices@users.noreply.github.com>
2026-02-17 16:10:56 +0100

    deleted keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,23 +8,16 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
-import keystatic from '@keystatic/astro'
 
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lemzanews.com',
-  output: 'server',
-  integrations: [mdx(), sitemap(), react(), markdoc(), keystatic()],
+  output: 'static',
+  integrations: [mdx(), sitemap(), react(), markdoc()],
 
   vite: {
     plugins: [tailwindcss()],
   },
-
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: false, // Kjo ndalon simulimin që po jep error
-    },
-  }),
 });
